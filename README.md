0c1f19a406460a19d29a2b4e25332c0c430e5fb3
g.skorupa@gmail.com <>
2019-07-11 20:17:08 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Living Documentation Docklet
+
+LivingDocDoclet is a Javadoc Doclet inspired by concepts from the "Living Documentation" book by Cyrille Martraire.
+
+## Introduction
+
+## Documenting code
+
+## Usage
+
+### Command line
+
+### Ant
+
+### Doclet options
+
+-file <file>
+  
+  Sets the output file location,
+
+-type <doctype>
---
+++ src/pl/com/experiot/livingdocumentation/architecture/HexagonalPort.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (C) Grzegorz Skorupa 2018.
+ * Distributed under the MIT License (license terms are at http://opensource.org/licenses/MIT).
+ */
+package pl.com.experiot.livingdocumentation.architecture;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ *
+ * @author greg
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface HexagonalPort {
+    
+}
